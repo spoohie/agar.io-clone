50573ef74c0b4b5e08cb10ec45c7059256dd6ed9
mechanicznapomarancza <kzawadka@gmail.com>
2018-06-04 19:28:38 +0200

    fix setFillColor

--- Menu.cpp
+++ Menu.cpp
@@ -9,17 +9,17 @@ Menu::Menu(float width, float height)
 	}
 
 	menu[0].setFont(font);
-	menu[0].setColor(sf::Color::Red);
+	menu[0].setFillColor(sf::Color::Red);
 	menu[0].setString("Play");
 	menu[0].setPosition(sf::Vector2f(width / 2, height / (NUMOFITEMS + 1) * 1));
 
 	menu[1].setFont(font);
-	menu[1].setColor(sf::Color::White);
+	menu[1].setFillColor(sf::Color::White);
 	menu[1].setString("Options");
 	menu[1].setPosition(sf::Vector2f(width / 2, height / (NUMOFITEMS + 1) * 2));
 
 	menu[2].setFont(font);
-	menu[2].setColor(sf::Color::White);
+	menu[2].setFillColor(sf::Color::White);
 	menu[2].setString("Exit");
 	menu[2].setPosition(sf::Vector2f(width / 2, height / (NUMOFITEMS + 1) * 3));
 
@@ -43,9 +43,9 @@ void Menu::moveUp()
 {
 	if (select - 1 >= 0)
 	{
-		menu[select].setColor(sf::Color::White);
+		menu[select].setFillColor(sf::Color::White);
 		select--;
-		menu[select].setColor(sf::Color::Red);
+		menu[select].setFillColor(sf::Color::Red);
 	}
 }
 
@@ -53,9 +53,9 @@ void Menu::moveDown()
 {
 	if (select + 1 < NUMOFITEMS)
 	{
-		menu[select].setColor(sf::Color::White);
+		menu[select].setFillColor(sf::Color::White);
 		select++;
-		menu[select].setColor(sf::Color::Red);
+		menu[select].setFillColor(sf::Color::Red);
 	}
 }
 
