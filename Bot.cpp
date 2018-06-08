2e9ec1d587ed741748f723c1597d1a6059a45a02
tizgane <kplatkowski@outlook.com>
2018-06-08 19:26:50 +0200

    bot v2 - goni gdy mniejszy, spierdala gdy wiekszy

--- Bot.cpp
+++ Bot.cpp
@@ -12,36 +12,9 @@ void Bot::movement(sf::RenderWindow& window, Player& pilka)
 
         if ((distanceTo(pilka.returnPosition())-r_-pilka.returnRadius()) < chasingDistance) 
         {
-
             shape_.move(-direction.x*velocity*botVelocityFactor, -direction.y*velocity*botVelocityFactor);
-            /*
-            diff_x = distanceTo(pilka) static_cast<float>(playerPosition.x) - shape_.getPosition().x;
-            diff_y = static_cast<float>(playerPosition.y) - shape_.getPosition().y;
-
-
-            shape_.move()*/
-        
-            std::cout << "chuj " << ++test << std::endl;
         }
 
     } else shape_.move(direction.x*velocity*botVelocityFactor, direction.y*velocity*botVelocityFactor);
-
-
-    /*
-    if (player_r > 2*r_)
-    {
-        diff_x = static_cast<float>(playerPosition.x) - shape_.getPosition().x;
-        diff_y = static_cast<float>(playerPosition.y) - shape_.getPosition().y;
-        if(sqrt(pow(diff_x, 2.0)+pow(diff_y, 2.0)) > 100.0)
-        {
-            shape_.move(velocity*diff_x*0.8,velocity*diff_y*0.8);
-        } else shape_.move(velocity*diff_x,velocity*diff_y);
-    }
-    else
-    {
-        shape_.move(0, 0);
-    }*/
-}
-
 
 Bot::~Bot() {}
